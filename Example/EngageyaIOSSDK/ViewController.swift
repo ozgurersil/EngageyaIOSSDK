1ea78b4356cf9613761cffa20bb09c74a58b41ad
Engageya <engageya@Engageyas-MacBook-Air.local>
2017-01-04 23:21:17 +0300

    hide ids

--- Example/EngageyaIOSSDK/ViewController.swift
+++ Example/EngageyaIOSSDK/ViewController.swift
@@ -17,21 +17,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         
-        let idS:[String:String] = [
-            "pub_id" : "164473",
-            "web_id" : "127410",
-            "wid_id" : "92644",
-            "url" : "http://www.haberturk.com/spor/futbol/haber/938402-ultraslandan-tffye-cikarma"
-        ]
         
-        
-        en.createWidget(idCollection: idS, creativeType: .tableView, align: .vertical) { (view) in
-            DispatchQueue.main.async {
-                if let tableView:UITableView = view as? UITableView{
-                    self.view.addSubview(tableView)
-                }
-            }
-        }
     }
     
     override func didReceiveMemoryWarning() {
